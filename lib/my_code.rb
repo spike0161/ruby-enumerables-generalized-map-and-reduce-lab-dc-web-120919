f090b289d9a8d1a96ff30b5c1dea3daee9c2794c
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-09-11 15:35:59 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,40 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length 
+  new.push(yield(array[i]))
+  i+= 1
+end
+  new
+end
+
+
+
+
+def reduce(array, starting_value = nil)
+  
+  if starting_value
+    sum = starting_value
+    i = 0
+  else 
+    sum = array[0]
+    i = 1
+  end
+  
+  while i < array.length
+  
+  sum = yield(sum, array[i])
+  i += 1
+end
+sum
+end
+
+
+
+
+
+
+
+
+
+
