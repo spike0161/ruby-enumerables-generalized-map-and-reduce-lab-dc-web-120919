223a26ff5bc601eaa1b656a1cfaa5419e61028cf
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-10-07 15:52:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,25 +12,23 @@ end
 
 
 
-def reduce(array, starting_value = nil)
-  
-  if starting_value
-    sum = starting_value
-    i = 0
-  else 
-    sum = array[0]
-    i = 1
-  end
-  
-  while i < array.length
-  
-  sum = yield(sum, array[i])
-  i += 1
-end
-sum
-end
-
-end
-  
-end
+  def reduce(array, starting_value = nil)
+    
+    if starting_value
+      sum = starting_value
+      i = 0
+    else 
+      sum = array[0]
+      i = 1
+    end
+    
+    while i < array.length
+    
+    sum = yield(sum, array[i])
+    i += 1
+        end
+          sum
+        end
+      end
+    end
 
